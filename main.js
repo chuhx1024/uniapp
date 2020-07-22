cce568bcc80ce0ca70503b61e1fa174e45ccb4a9
chuhx <hx.chu@clustar.ai>
2020-07-22 17:54:40 +0800

    feat: 添加.gitignore  项目引入 uview

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ import App from './App'
 
 Vue.config.productionTip = false
 
+import uView from "uview-ui"
+Vue.use(uView)
+
 App.mpType = 'app'
 
 const app = new Vue({
--- pages/message/index.vue
+++ pages/message/index.vue
@@ -1,13 +1,7 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			123444444
-			<text class="title">{{title}}</text>
-			<text class="title">{{title}}</text>
-			<text class="title">{{title}}</text>
-			<text class="title">{{title}}</text>
-		</view>
+		<u-button @click="showModle">点赞</u-button>
+		<u-action-sheet :list="list" v-model="show"></u-action-sheet>
 	</view>
 </template>
 
@@ -15,42 +9,22 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello',
+				list: [{
+					text: '点赞',
+					color: 'blue',
+					fontSize: 28
+				}, {
+					text: '分享'
+				}, {
+					text: '评论'
+				}],
+				show: false
 			}
 		},
-		onLoad() {
-
-		},
 		methods: {
-
+			showModle () {
+				this.show = true
+			}
 		}
 	}
 </script>
-
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
-</style>
